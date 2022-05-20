213ba442bfbfcdf43de6d2b742b174aeb4ca5424
Yago Vega <YagoVega9Nk@gmail.com>
2022-05-19 20:34:32 -0500

    .

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require ("express");
 const app = express();
 const server = 8080;
 
+app.set("view engine", "pug");
+
+app.get("/",(req, res)=>{
+    res.render("index");
+});
+
 app.listen(server, ()=>{
     console.log("Server launched");
 });
