38ddc9b36e9905a123eb6d62893a06e508eb53e1
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2022-05-19 23:36:34 -0500

    Removi el app.js duplicado

--- Papelerias Estefania/src/app.js
+++ Papelerias Estefania/src/app.js
@@ -1,6 +1,13 @@
 const express = require ("express");
 const app = express();
+const server = 8080;
+
+app.set("view engine", "pug");
+
+app.get("/",(req, res)=>{
+    res.render("index");
+});
 
 app.listen(server, ()=>{
-
+    console.log("Server launched");
 });
