f60da175cab9b935ee61da87e56240c3350900a7
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2022-05-20 16:13:30 -0500

    Sumary

--- app.js
+++ app.js
@@ -4,10 +4,70 @@ const port = 8080;
 
 app.set("view engine", "pug");
 
+//--- Index ----------------------------------------------------------------------------
 app.get("/",(req, res)=>{
     res.render("index");
 });
 
+//--- Empleados ------------------------------------------------------------------------
+//Menu Empleado
+app.get("/empleado/menu",(req, res)=>{
+    res.render("");
+});
+
+//Contacto con proveedores
+app.get("/empleado/proovedores",(req, res)=>{
+    res.render("");
+});
+
+//Productos especiales en stock
+app.get("/empleado/especiales",(req, res)=>{
+    res.render("");
+});
+
+//Pedidos recientes
+app.get("/empleado/pedidos",(req, res)=>{
+    res.render("");
+});
+
+//Inventario
+app.get("/empleado/inventario",(req, res)=>{
+    res.render("");
+});
+
+//--- Clientes ------------------------------------------------------------------------
+//Menu Cliente
+app.get("/cliente/menu",(req, res)=>{
+    res.render("");
+});
+
+//Productos de escritura
+app.get("/cliente/poductos/escritura",(req, res)=>{
+    res.render("");
+});
+
+//Productos de papel
+app.get("/cliente/poductos/papeleria",(req, res)=>{
+    res.render("");
+});
+
+//Productos de medicion
+app.get("/cliente/poductos/medicion",(req, res)=>{
+    res.render("");
+});
+
+//Productos de manualidades
+app.get("/cliente/poductos/manualidades",(req, res)=>{
+    res.render("");
+});
+
+//Canasta
+app.get("/cliente/canasta",(req, res)=>{
+    res.render("");
+});
+
+
+//Lanzar servidor
 app.listen(port, ()=>{
     console.log("Server launched in the port "+port);
 });
