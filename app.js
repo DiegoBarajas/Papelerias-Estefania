64b974528e7084bfbc5f661e6be37da7d76ed53c
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2022-05-24 21:05:17 -0500

    Update to CSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const port = 8080;
 
+app.use("/public",express.static("public"));
+
 app.set("view engine", "pug");
 
 //--- Index ----------------------------------------------------------------------------
